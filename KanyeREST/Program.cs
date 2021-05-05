f340c42390938e33a5be12c632b3f3ed7a315d0c
Kemuro <antonkemuro@gmail.com>
2021-05-05 18:56:40 -0500

    created kanyeREST

---
+++ KanyeREST/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeREST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for(int i = 0; i < 20; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    QuoteGenerator.KanyeQuote();
+                }
+                else
+                {
+                    QuoteGenerator.RonQuote();
+                }
+            }
+        }
+    }
+}
